a4291fd54e9edac420d15669476b4d9583a32c36
Shivam Kumar Manjhi <113127627+Imshivam1@users.noreply.github.com>
2025-01-14 23:35:59 +0530

    added prac files

---
+++ DataTypes.java
@@ -0,0 +1,21 @@
+public class DataTypes {
+    public static void main(String[] args) {
+        byte b = 120;
+        short s = 32000;
+        int i = 100000;
+        long l = 10000000000L;
+        float f = 10.5f;
+        double d = 20.99;
+        char c = 'A';
+        boolean bool = true;
+
+        System.out.println("byte: " + b);
+        System.out.println("short: " + s);
+        System.out.println("int: " + i);
+        System.out.println("long: " + l);
+        System.out.println("float: " + f);
+        System.out.println("double: " + d);
+        System.out.println("char: " + c);
+        System.out.println("boolean: " + bool);
+    }
+}
---
+++ MyName.java
@@ -0,0 +1,11 @@
+public class MyName {
+    public static void main(String[] args) {
+        String name = "Shivam Kumar Manjhi";
+        int age = 24;
+        String message = "Hello everyone, welcome to my first Java programming!";
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Message: " + message);
+    }
+}
