13efe8d62b8bcba108d03556f5b7139ed17593e7
Shivam Kumar Manjhi <113127627+Imshivam1@users.noreply.github.com>
2025-01-14 17:07:11 +0530

    a

--- Demo.java
+++ Demo.java
@@ -9,7 +9,7 @@ class Demo {
     public static void main(String[] args) { // Main method
         Demo d = new Demo(); // Create Demo object
         d.a = 6;             // Assign values to variables
-        d.b = 10;
+        d.b = 12;
         d.show();            // Call the show method
     }
 }
