adffeae5497dc56c702a6ca7c8805d7197bdc737
Shivam Kumar Manjhi <113127627+Imshivam1@users.noreply.github.com>
2025-01-16 15:11:51 +0530

    added relationalOperators

---
+++ assignment 2/RelationalOperators.java
@@ -0,0 +1,18 @@
+/* 4. Relational Operators
+What are Relational Operators?
+
+Operators that compare two values and return a boolean (true or false).
+Examples: ==, !=, >, <, >=, <=.
+*/
+public class RelationalOperators {
+    public static void main(String[] args) {
+        int a = 10, b = 20;
+
+        System.out.println("a == b: " + (a == b));
+        System.out.println("a != b: " + (a != b));
+        System.out.println("a > b: " + (a > b));
+        System.out.println("a < b: " + (a < b));
+        System.out.println("a >= b: " + (a >= b));
+        System.out.println("a <= b: " + (a <= b));
+    }
+}
