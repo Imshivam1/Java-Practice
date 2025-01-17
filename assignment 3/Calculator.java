6b8c23b756a84e9b1855309ac849d83b19180510
Shivam Kumar Manjhi <113127627+Imshivam1@users.noreply.github.com>
2025-01-17 12:23:45 +0530

    Calculator

---
+++ assignment 3/Calculator.java
@@ -0,0 +1,41 @@
+/*
+Task 1: Basic Calculator
+Explanation: This program takes two numbers and an operator (+, -, *, /) as input. 
+Based on the operator, it performs the corresponding calculation using a switch statement.
+*/
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Input: Two numbers and an operator
+        System.out.println("Enter the first number: ");
+        double num1 = sc.nextDouble();
+        System.out.println("Enter the second number: ");
+        double num2 = sc.nextDouble();
+        System.out.println("Enter an operator (+, -, *, /): ");
+        char operator = sc.next().charAt(0);
+
+        // Switch to perform operations
+        switch (operator) {
+            case '+':
+                System.out.println("Result: " + (num1 + num2));
+                break;
+            case '-':
+                System.out.println("Result: " + (num1 - num2));
+                break;
+            case '*':
+                System.out.println("Result: " + (num1 * num2));
+                break;
+            case '/':
+                if (num2 != 0)
+                    System.out.println("Result: " + (num1 / num2));
+                else
+                    System.out.println("Error: Division by zero is not allowed.");
+                break;
+            default:
+                System.out.println("Invalid operator!");
+        }
+    }
+}
