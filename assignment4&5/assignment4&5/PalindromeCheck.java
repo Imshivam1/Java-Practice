b03883489775c678489f7ae1353a9b2e70a5f213
Shivam Kumar Manjhi <113127627+Imshivam1@users.noreply.github.com>
2025-01-20 16:33:00 +0530

    Check pallindrome with comments

--- assignment4&5/assignment4&5/PalindromeCheck.java
+++ assignment4&5/assignment4&5/PalindromeCheck.java
@@ -1,12 +1,13 @@
 public class PalindromeCheck {
     public boolean isPalindrome(String str) {
-        String reversed = new StringBuilder(str).reverse().toString();
-        return str.equalsIgnoreCase(reversed);
+        String reversed = new StringBuilder(str).reverse().toString();// stores reversed str in reversed String by using reverse to convert
+        //and toString() to keep it in String format only
+        return str.equalsIgnoreCase(reversed);//compares both str and reversed igonring case sensitive letters
     }
 
     public static void main(String[] args) {
         PalindromeCheck pc = new PalindromeCheck();
-        System.out.println("Is palindrome: " + pc.isPalindrome("madam"));
-        System.out.println("Is palindrome: " + pc.isPalindrome("hello"));
+        System.out.println("Is palindrome: " + pc.isPalindrome("madam"));//checking if it is pallindrome or not and the result is true
+        System.out.println("Is palindrome: " + pc.isPalindrome("hello"));// result will be false
     }
 }
