b4ce611224e388e3b1d75e178e2102e7e52b5cf3
Shivam Kumar Manjhi <113127627+Imshivam1@users.noreply.github.com>
2025-01-20 17:29:59 +0530

    String Operations concatenated, substring, replace and case convert

--- assignment4&5/assignment4&5/StringOperations.java
+++ assignment4&5/assignment4&5/StringOperations.java
@@ -7,7 +7,7 @@ public class StringOperations {
         System.out.println("Concatenated: " + concatenated);
 
         // b. Substring
-        System.out.println("Substring: " + str.substring(0, 5));
+        System.out.println("Substring: " + str.substring(0, 5));//here oth index is included and 5th is not as it goes upto (n-1) value
 
         // c. Replace
         System.out.println("Replaced: " + str.replace('o', '0'));
