eea6a68b747203a429e198ce5bdff6d7998973d8
Shivam Kumar Manjhi <113127627+Imshivam1@users.noreply.github.com>
2025-01-31 23:00:45 +0530

    fibo

--- assignment 6/Fibo.java
+++ assignment 6/Fibo.java
@@ -17,3 +17,21 @@ public class Fibo {
         }
     }
 }
+//fibonacci series
+//0, 1, 1, 2, 3, 5, 8, 13
+//0, 1, 0+1, 1+1, 1+2,
+/*
+input
+2 =test case
+0 2 10 =a, b , n
+5 3 5
+output
+2 6 14 30 62 126 254 510 1022 2046
+8 14 26 50 98
+
+0 + 1.2 + 2.2 + 4.2 = 14
+term n = a+ b(2pow0 + 2 pow1 + 2 pow2 +....+ )
+j= n;
+current = a
+current += b * Math.pow(2, j);
+*/
