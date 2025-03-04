c282a69884c868e7abf17aa9e4ce7ccf9afc26c7
Shivam Kumar Manjhi <113127627+Imshivam1@users.noreply.github.com>
2025-03-04 14:28:34 +0530

    package example 1

---
+++ assignment 6/Pack1.java
@@ -0,0 +1,7 @@
+package mypackage; // Package declaration
+
+public class Pack1{
+    public void displayMessage() {
+        System.out.println("Hello from mypackage!");
+    }
+}
---
+++ assignment 6/Test1.java
@@ -0,0 +1,8 @@
+import mypackage.Pack1;
+
+public class Test1{
+    public static void main(String[] args){
+        Pack1 obj = new Pack1();
+        obj.displayMessage();
+    }
+}
