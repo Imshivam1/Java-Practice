63f20ccb9cbfb5219e814b5e47e5784b1fbcd6ef
Shivam Kumar Manjhi <113127627+Imshivam1@users.noreply.github.com>
2025-03-04 14:42:17 +0530

    added package ex

--- assignment 6/Test1.java
+++ assignment 6/Test1.java
@@ -1,8 +1,12 @@
+import static java.lang.Math.*;
 import mypackage.Pack1;
 
 public class Test1{
     public static void main(String[] args){
         Pack1 obj = new Pack1();
         obj.displayMessage();
+        int i = 16;
+        System.out.println(sqrt(i));
     }
+
 }
