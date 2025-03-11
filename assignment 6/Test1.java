6e5ec57d18aca86f9375bc9a565d4acb9cd12a99
Shivam Kumar Manjhi <113127627+Imshivam1@users.noreply.github.com>
2025-03-11 14:06:49 +0530

    chnages package

--- assignment 6/Test1.java
+++ assignment 6/Test1.java
@@ -5,7 +5,7 @@ public class Test1{
     public static void main(String[] args){
         Pack1 obj = new Pack1();
         obj.displayMessage();
-        int i = 16;
+        int i = 18;
         System.out.println(sqrt(i));
     }
 
